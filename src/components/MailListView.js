a0fed82edf6c88fac9bfd3ec2fdfc584e42d1652
Raghav <singh.raghverndra@pwc.com>
2017-02-11 18:02:04 +0800

    Decoded message from gmail API

--- src/components/MailListView.js
+++ src/components/MailListView.js
@@ -1,27 +1,99 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
+require('normalize.css/normalize.css')
+require('styles/App.css')
 
-import React from 'react';
-import actions from '../actions'
+import React from 'react'
+// import actions from '../actions'
 import { connect } from 'react-redux'
+import utf8 from 'utf8'
+const userId = 'singh.raghverndra@businessos.net'
 
 class MailListView extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      messageAndThreadIds: []
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.listMessages(userId, 'businessos', (results) => {
+        this.setState({
+          messageAndThreadIds: results
+        })
+      })
+    }, 3000)
+  }
+
+  listMessages(userId, query, callback) {
+    let getPageOfMessages = (request, result) => {
+      request.execute(function(resp) {
+        result = result.concat(resp.messages)
+        let nextPageToken = resp.nextPageToken
+        // Deliberately restricted multiple calls since it was an POC
+        // In actual code hardcoded pageToken ('02715675988415759326')
+        // should be removed.
+        if (nextPageToken == '02715675988415759326') {
+          request = window.gapi.client.gmail.users.messages.list({
+            'userId': userId,
+            'pageToken': nextPageToken,
+            'q': query
+          });
+          getPageOfMessages(request, result)
+        } else {
+          callback(result)
+        }
+      })
+    }
+    let initialRequest = window.gapi.client.gmail.users.messages.list({
+      'userId': userId,
+      'q': query
+    })
+    getPageOfMessages(initialRequest, [])
+  }
+
+  getMessage(userId, messageId, format, callback) {
+    var request = window.gapi.client.gmail.users.messages.get({
+      'userId': userId,
+      'id': messageId,
+      'format': format
+    });
+    request.execute(callback)
+  }
+
+  decodeUrlSafeBase64(s) {
+    return atob(s.replace(/\-/g, '+').replace(/\_/g, '/'))
+  }
+
   render() {
+    let messages = this.state.messageAndThreadIds.splice(1, 1)
+    messages.length > 0 ? (
+      messages.map((value) => {
+        this.getMessage(userId, value.id, 'raw', (result) => {
+          console.log(result) // log whole result object
+          console.log(this.decodeUrlSafeBase64(result.raw)) // log whole message <HTML> data
+          console.log(utf8.decode(this.decodeUrlSafeBase64(result.raw))) // log whole email data with utf8 encoding
+        })
+      })
+    ) : null
     return (
       <div className="index" style={styles.container} >
         <h4>MailListView here</h4>
+        {
+
+        }
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = () => {
   // return { auth: state.auth, }
   return {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = () => {
   return {
   }
 }
