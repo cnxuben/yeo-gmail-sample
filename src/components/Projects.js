c7e57c11cb0adf468dc5d29402abe2e596c1d967
ryuan018 <ryan.yuan@pwc.com>
2017-02-15 14:54:35 +0800

    proj ui update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import { Icon  } from 'antd';
 
+
+const mockProjectList = [
+  {
+    key:1,
+    title: 'Now',
+    items: [
+      {
+        key:1,
+        img:'',
+        projectName: 'RAC Black Friday',
+        description: 'There some text',
+        updates:2,
+        during: 'Aug, 4 - Aug, 25',
+        person: [
+          {
+            img:'asd'
+          }
+        ]
+      }
+    ]
+  }
+]
+
 export default class ProjectsView extends React.Component{
   constructor(props){
     super(props)
@@ -16,7 +39,9 @@ export default class ProjectsView extends React.Component{
     return (
       <div style={{height:'100%',overflowY:'scroll'}} >
         <h3>Title</h3>
-
+        <div className="project-container" style={{background:'red'}}>
+          sss
+        </div>
       </div>
     )
 
